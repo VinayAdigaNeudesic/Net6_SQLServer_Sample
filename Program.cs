8ea1c8c380939d0da6ced67cbafc092e8dca3c55
Vinay Adiga <Vinay.Adiga@assurant.com>
2022-08-18 10:43:23 +0530

    swagger for all

--- Program.cs
+++ Program.cs
@@ -23,11 +23,8 @@ namespace Sample_API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             if (app.Environment.IsDevelopment())
             {
